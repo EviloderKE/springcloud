db6edddc964092a0d4b83e32bc3f8c252a75da9a
ke <447511881@qq.com>
2020-12-22 20:36:49 +0800

    init

---
+++ order/src/main/java/com/example/order/controller/OrderController.java
@@ -0,0 +1,23 @@
+package com.example.order.controller;
+
+import com.example.common.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RequestMapping("order")
+@RestController
+public class OrderController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("get")
+    public User get(){
+        String url = "http://localhost:9000/payment/test";
+        return restTemplate.getForObject(url, User.class);
+    }
+
+}
