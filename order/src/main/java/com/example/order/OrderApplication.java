9115380e70228b3128e94abf0a917a6078b8ee65
ke <447511881@qq.com>
2020-12-27 13:33:11 +0800

    ribbon 配置负载规则

--- order/src/main/java/com/example/order/OrderApplication.java
+++ order/src/main/java/com/example/order/OrderApplication.java
@@ -1,9 +1,12 @@
 package com.example.order;
 
+import com.example.ribbonRule.RibbonRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
+@RibbonClient(name = "payment", configuration = RibbonRule.class)
 public class OrderApplication {
 
     public static void main(String[] args) {
