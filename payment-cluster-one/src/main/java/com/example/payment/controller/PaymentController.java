9a7f8cfe5eeb46dcd70440e9496e265f8600ff0d
ke <447511881@qq.com>
2020-12-30 21:35:00 +0800

    hystrix 服务熔断

--- payment-cluster-one/src/main/java/com/example/payment/controller/PaymentController.java
+++ payment-cluster-one/src/main/java/com/example/payment/controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.example.common.entities.User;
 import com.example.payment.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,11 @@ public class PaymentController {
     @GetMapping("timeout")
     public String timeOut(){
         return paymentService.timeOut();
+    }
+
+    @GetMapping("break/{id}")
+    public String paymentCircuitBreaker(@PathVariable("id") int id){
+        return paymentService.paymentCircuitBreaker(id);
     }
 
 }
--- payment-cluster-one/src/main/java/com/example/payment/service/PaymentService.java
+++ payment-cluster-one/src/main/java/com/example/payment/service/PaymentService.java
@@ -4,6 +4,7 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -23,6 +24,30 @@ public class PaymentService {
 
     public String myFallBack(){
         return "myFallBack";
+    }
+
+    /**
+     * 服务的降级->进而熔断->恢复调用链路
+     *
+     * @param id
+     * @return
+     */
+    @HystrixCommand(fallbackMethod = "paymentCircuitBreakerFallback", commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled", value = "true"), //是否开启断路器
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"), //请求次数
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"), //时间窗口期
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60"),//失败率达到多少后跳闸
+    })
+    public String paymentCircuitBreaker(Integer id) {
+        if (id < 0) {
+            throw new RuntimeException("******id 不能为负数");
+        }
+        String serialNumber = UUID.randomUUID().toString();
+        return Thread.currentThread().getName() + "\t" + "调用成功，流水号：" + serialNumber;
+    }
+
+    public String paymentCircuitBreakerFallback(Integer id) {
+        return "请稍后再试";
     }
 
 
