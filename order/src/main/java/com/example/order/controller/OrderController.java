ade3df7e3f2759d245b11dd7580e8b77ff5aa0a2
ke <447511881@qq.com>
2021-01-01 16:55:30 +0800

    config 单体刷新

--- order/src/main/java/com/example/order/controller/OrderController.java
+++ order/src/main/java/com/example/order/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.example.order.controller;
 
 import com.example.common.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,15 +11,24 @@ import org.springframework.web.client.RestTemplate;
 
 @RequestMapping("order")
 @RestController
+@RefreshScope
 public class OrderController {
 
     @Autowired
     private RestTemplate restTemplate;
 
+    @Value("${version}")
+    private String version;
+
     @GetMapping("get")
     public User get(){
         String url = "http://PAYMENT/payment/test";
         return restTemplate.getForObject(url, User.class);
+    }
+
+    @GetMapping("test")
+    public String test(){
+        return version;
     }
 
 }
