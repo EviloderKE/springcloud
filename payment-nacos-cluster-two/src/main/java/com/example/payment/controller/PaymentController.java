2f268b57dc7e1989f086fb6312f6ec5150645e27
ke <447511881@qq.com>
2021-01-02 14:03:50 +0800

    nacos payment

---
+++ payment-nacos-cluster-two/src/main/java/com/example/payment/controller/PaymentController.java
@@ -0,0 +1,18 @@
+package com.example.payment.controller;
+
+import com.example.common.entities.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+
+    @GetMapping("/test")
+    public User test(){
+        return new User("zk", 2);
+    }
+
+
+}
