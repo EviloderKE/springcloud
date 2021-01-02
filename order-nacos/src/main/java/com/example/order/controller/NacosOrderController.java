0a837178a16961798ed8982b6729d82e541af9db
ke <447511881@qq.com>
2021-01-02 15:45:42 +0800

    nacos order config

--- order-nacos/src/main/java/com/example/order/controller/NacosOrderController.java
+++ order-nacos/src/main/java/com/example/order/controller/NacosOrderController.java
@@ -3,20 +3,31 @@ package com.example.order.controller;
 import com.example.common.entities.User;
 import com.example.order.service.FeignNacosService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("order")
 @RestController
+@RefreshScope
 public class NacosOrderController {
 
     @Autowired
-    private FeignNacosService feignService;
+    private FeignNacosService feignNacosService;
+
+    @Value("${v}")
+    private String v;
 
     @GetMapping("get")
     public User get(){
-        return feignService.test();
+        return feignNacosService.test();
+    }
+
+    @GetMapping("version")
+    public String version(){
+        return this.v;
     }
 
 }
